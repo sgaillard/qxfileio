a6fa3a4ded2d26b0cfaa3d4a91e03a5b9c822664
Sylvain Gaillard <sylvain.gaillard@angers.inra.fr>
2015-11-04 11:23:55 +0100

    New qxFileIO project

---
+++ source/class/qxfileio/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxfileio.theme.Theme",
+{
+  meta :
+  {
+    color : qxfileio.theme.Color,
+    decoration : qxfileio.theme.Decoration,
+    font : qxfileio.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxfileio.theme.Appearance
+  }
+});
