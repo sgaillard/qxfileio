14a68f90372cad2bf1a17f174f34f76431caae33
Sylvain Gaillard <sylvain.gaillard@angers.inra.fr>
2016-01-26 12:00:10 +0100

    Moved things to get a better integration of the HTML input element

--- source/class/qxfileio/FileChooser.js
+++ source/class/qxfileio/FileChooser.js
@@ -23,38 +23,70 @@ qx.Class.define("qxfileio.FileChooser",
     {
       check : "Boolean",
       init : false,
-      event : "multipleSelectChange"
+      event : "multipleSelectChange",
+      apply : "__applyMultipleSelect"
     }
+  },
+
+  construct : function() {
+    // Element creation
+    this.__inputEl = document.createElement("input");
+    this.__inputEl.type = "file";
+    this.__inputEl.style.position = "absolute";
+    this.__inputEl.style.hideFocus = "true";
+    this.__inputEl.style.zIndex = -10000;
+    this.__inputEl.style.left = "-1000px";
+    this.__inputEl.style.width = "1px";
+
+    // Event handling
+    this.__inputEl.onchange = (function(me) {
+      return function(e) {
+        //me.debug("!!! File changed [" + e.target.files.length + "] !!!");
+        me.fireDataEvent('filesChange', e.target.files);
+      };
+    })(this);
+    this.__inputEl.onclick = (function(me) {
+      return function(e) {
+        document.body.removeChild(e.target);
+        //me.debug("!!! Removed !!!");
+      }
+    })(this);
   },
 
   members :
   {
     /**
+     * The embeded HTML input type file element.
+     */
+    __inputEl : null,
+
+    /**
+     * Apply the value of multipleSelect to the HTML input element.
+     */
+    __applyMultipleSelect : function(val) {
+      this.debug("multiple: " + val);
+      if (val) {
+        this.__inputEl.multiple = "multiple";
+      } else {
+        this.debug("remove multiple");
+        this.__inputEl.multiple = "";
+      }
+    },
+
+    /**
      * Opens the 'open file' dialog box.
      */
     open : function() {
-      var inputEl = document.createElement("input");
-      inputEl.type = "file";
-      if (this.getMultipleSelect()) {
-        inputEl.multiple = "multiple";
-      }
-      inputEl.style.position = "absolute";
-      inputEl.style.hideFocus = "true";
-      inputEl.style.zIndex = -10000;
-      inputEl.style.left = "-1000px";
-      inputEl.style.width = "1px";
-      inputEl.onchange = (function(me) {
-        return function(e) {
-          me.fireDataEvent('filesChange', e.target.files);
-        }
-      })(this);
-      inputEl.onclick = (function(me) {
-        return function(e) {
-          document.body.removeChild(e.target);
-        }
-      })(this);
-      document.body.appendChild(inputEl);
-      inputEl.click();
+      document.body.appendChild(this.__inputEl);
+      this.__inputEl.click();
+    },
+
+    /**
+     * Get the file list
+     * @return {FileList} the list of File
+     */
+    getFiles : function() {
+      return this.__inputEl.files;
     }
   }
 });
