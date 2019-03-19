1097efd56d3fa27686b484b3be4c16f2cfd77b06
Sylvain Gaillard <sylvain.gaillard@inra.fr>
2019-03-19 09:18:22 +0100

    Api doc for StringTokenizer

--- source/class/qxfileio/StringTokenizer.js
+++ source/class/qxfileio/StringTokenizer.js
@@ -1,8 +1,35 @@
 /**
  * A String tokenizer that iterates over a String and retruns substrings.
+ *
+ * Usage:
+ * <pre class='javascript'>
+ * var text = "This is a text";
+ * var st = new qxfileio.StringTokenizer(text, " ");
+ * while (st.hasMoreToken()) {
+ *   var t = st.nextToken();
+ *   console.log(`[${t}]`);
+ * }
+ * </pre>
+ * That should print:
+ * <pre>
+ * [This]
+ * [is]
+ * [a]
+ * [text]
+ * </pre>
  */
 qx.Class.define("qxfileio.StringTokenizer", {
   extend : qx.core.Object,
+
+  /**
+   * Constructor
+   *
+   * @param str {String} The string object to tokenize. If it is a primitive
+   * string type, it will be copied into a String object. If it is a String
+   * object it will be referenced but not copied.
+   * @param split {String} The split mark on witch tokens will be separated.
+   * Default value: "\n". It is removed from the tokens.
+   */
   construct : function(str, split = "\n") {
     this.base(arguments);
     this.__str = new String(str);
@@ -10,23 +37,35 @@ qx.Class.define("qxfileio.StringTokenizer", {
     this.__length = str.length;
     this.__split = split;
   },
+
   members : {
     __str : null,
     __lastPos : 0,
     __length : 0,
     __split : "\n",
 
+    /**
+     * Test used to know if there is some remainig token (i.e. if the string
+     * had been completely parsed).
+     *
+     * @return {Bool} True if there is more token in the string
+     */
     hasMoreToken : function() {
       return this.__lastPos != this.__length;
     },
 
+    /**
+     * Get then next token in the string.
+     *
+     * @return {String} The token
+     */
     nextToken : function() {
       var actualPos = this.__lastPos;
-      var nextPos = this.str.indexOf(this.__split, actualPos);
+      var nextPos = this.__str.indexOf(this.__split, actualPos);
       if (nextPos == -1) {
         nextPos = this.__length;
       }
-      var token = this.__str.substsring(actualPos, nextPos);
+      var token = this.__str.substring(actualPos, nextPos);
       if (nextPos == this.__length) {
         this.__lastPos = nextPos;
       } else {
@@ -35,16 +74,30 @@ qx.Class.define("qxfileio.StringTokenizer", {
       return token;
     },
 
+    /**
+     * Get the N next tokens in the string
+     *
+     * If the number of tokens requested is higher than the number of remaining
+     * tokens in the string, empty string tokens will be returned. The returned
+     * Array will always have the requested size.
+     *
+     * @param n {integer} The number of tokens to get.
+     *
+     * @return {Array} A Array of tokens
+     */
     nextNTokens : function(n) {
       var tokens = [];
       for (var i = 0 ; i < n ; i++) {
-        tokens.append(this.nextToken());
+        tokens.push(this.nextToken());
       }
       return tokens;
     },
 
+    /**
+     * Get back to the beginning of the string.
+     */
     rewind : function() {
       this.__lastPos = 0;
     }
   }
-}
+});
